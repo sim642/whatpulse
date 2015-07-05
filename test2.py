008007ec71364e0caf7e0b6cf93212ac05061d8b
Simmo Saan <simmo.saan@gmail.com>
2015-07-05 16:59:51 +0300

    Add .gitignore

--- test2.py
+++ test2.py
@@ -5,4 +5,4 @@ wp.try_login('whattester@mailinator.com', 'whattester')
 wp.login('py')
 wp.client_login()
 
-wp.pulse(whatpulse.Stats(clicks=10, download=5, upload=1, uptime=100))
+#wp.pulse(whatpulse.Stats(clicks=10, download=5, upload=1, uptime=100))

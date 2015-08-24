d624fbb4d9981a8c0e28596930d16a2f849310f0
Simmo Saan <simmo.saan@gmail.com>
2015-08-24 14:33:59 +0300

    Patcher: allow forcing newline setting

--- patcher/patcher.py
+++ patcher/patcher.py
@@ -1,6 +1,20 @@
-# TODO: choice to handle \r\n line endings instead for .exe
+import sys, re
+
 search = open('search.pem', 'rb').read()
 replace = open('replace.pem', 'rb').read()
+
+def nlfix(buf, newnl):
+	return re.sub(b'\r?\n', newnl, buf)
+
+newnl = None
+if sys.argv[1] == "lf":
+	newnl = b'\n'
+elif sys.argv[1] == "crlf":
+	newnl = b'\r\n'
+
+if newnl:
+	search = nlfix(search, newnl)
+	replace = nlfix(replace, newnl)
 
 replace = replace.ljust(len(search), b'\x00')
 
